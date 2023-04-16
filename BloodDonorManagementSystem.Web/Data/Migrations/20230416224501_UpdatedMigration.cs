621ef439d37f3fe5c0fae1f03f2bde2060fd83bf
josephLee2022 <98351922+josephLee2022@users.noreply.github.com>
2023-04-16 17:51:56 -0500

    Updated CRUD

---
+++ BloodDonorManagementSystem.Web/Data/Migrations/20230416224501_UpdatedMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BloodDonorManagementSystem.Web.Data.Migrations
+{
+    public partial class UpdatedMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateUpdated",
+                table: "BloodTypes",
+                newName: "DateModified");
+
+            migrationBuilder.RenameColumn(
+                name: "DateUpdated",
+                table: "BloodProductsAllocations",
+                newName: "DateModified");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateModified",
+                table: "BloodTypes",
+                newName: "DateUpdated");
+
+            migrationBuilder.RenameColumn(
+                name: "DateModified",
+                table: "BloodProductsAllocations",
+                newName: "DateUpdated");
+        }
+    }
+}
