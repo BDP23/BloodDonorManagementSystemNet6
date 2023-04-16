eeea3b1cce165384dc6be2f1625c57385f4e23a6
josephLee2022 <98351922+josephLee2022@users.noreply.github.com>
2023-04-16 16:22:43 -0500

    Add project files.

---
+++ BloodDonorManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonorManagementSystem.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BloodDonor>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ BloodDonorManagementSystem.Web/Data/BloodDonor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+namespace BloodDonorManagementSystem.Web.Data
+{
+    public class BloodDonor : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
+        public string? DonorId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateVisited { get; set; }
+
+    }
+}
